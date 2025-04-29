90f5828de13dabef69536459c30dec50f2ac9045
DhandhaDendriyaE <dandadendriya17@gmail.com>
2025-04-30 00:00:39 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+video.addEventListener("click", function video(){
+    alert("OTW TREBLE NIH, MU KAPAN?");
+});
+
+audio.addEventListener("click", function video(){
+    alert("SILAHKAN DIDENGARKAN BANG");
+});
+
+gambar.addEventListener("click", function video(){
+    alert("YA KALI BANG 12 TAHUN PUASA");
+});
+
+youtube.addEventListener("click", function video(){
+    alert("SILAHKAN DI TONTON BANG");
+});
+
+bonus.addEventListener("click", function video(){
+    alert("CANDA BANG WKWKWK, MU PASTI JUARA KOK KAPAN2");
+});
+
+    
